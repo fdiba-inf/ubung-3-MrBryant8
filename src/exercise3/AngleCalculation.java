e4b57f7969a3a5e18a0260ced10f52e01d395934
MrBryant8 <mincho.tanev@fdiba.tu-sofia.bg>
2021-10-31 16:27:10 +0200

    wassup

--- src/exercise3/AngleCalculation.java
+++ src/exercise3/AngleCalculation.java
@@ -4,25 +4,27 @@ import java.util.Scanner;
 
 public class AngleCalculation {
     public static void main(String[] args) {
-        Scanner input=new Scanner(System.in);
+        /*Scanner input = new Scanner(System.in);
 
-        double angle= input.nextDouble();
-        char type=input.next().charAt(0);
+        double angle = input.nextDouble();
+        char type = input.next().charAt(0);
 
         double angle1;
         double angle2;
-        do{
-            if(type == 'r'){
-                angle1 =angle / Math.PI * 180;
-                System.out.println("Angle: "+ angle1 + "d");
+        do {
+            if (type == 'r') {
+                angle1 = angle / Math.PI * 180;
+                System.out.println("Angle: " + angle1 + "d");
                 break;
-            }else if (type == 'd'){
-                angle2=angle * Math.PI / 180;
-                System.out.println("Angle: "+ angle2+ "r");
+            } else if (type == 'd') {
+                angle2 = angle * Math.PI / 180;
+                System.out.println("Angle: " + angle2 + "r");
                 break;
             }
 
-        }while(type == 'd' || type == 'r');
+        } while (type == 'd' || type == 'r');
+    }
+}*/
 
         //double angle,angle2, angle1 ;
         //char type;
@@ -41,5 +43,23 @@ public class AngleCalculation {
 
         }while(type =='r'|| type =='d');*/
 
+        Scanner input = new Scanner(System.in);
+
+        double value;
+        char type;
+        do {
+            value = input.nextDouble();
+            type = input.next().charAt(0);
+
+            if (type == 'r') {
+                double convertedValue = value / Math.PI * 180;
+                System.out.println("Angle: " + convertedValue + "d");
+            } else if (type == 'd') {
+                double convertedValue = value * Math.PI / 180;
+                System.out.println("Angle: " + convertedValue + "r");
+            }
+        } while (type == 'r' || type == 'd');
+
     }
 }
+
--- src/exercise3/Triangle.java
+++ src/exercise3/Triangle.java
@@ -4,13 +4,13 @@ import java.util.Scanner;
 
 public class Triangle {
     public static void main(String[] args) {
-        Scanner input=new Scanner(System.in);
+        /*Scanner input = new Scanner(System.in);
         int a = input.nextInt();
         int b = input.nextInt();
         int c = input.nextInt();
-        double area,p;
-        do{
-            boolean nas=a>0 && b>0 && c>0 && a+b>c && b+c>a && c+a>b;
+        double area, p;
+        do {
+            boolean nas = a > 0 && b > 0 && c > 0 && a + b > c && b + c > a && c + a > b;
             if (nas) {
 
                 p = (a + b + c) / 2;
@@ -20,20 +20,52 @@ public class Triangle {
                 if (a == b && b == c) {
                     System.out.println("Triangle: equilateral");
                     break;
-                } else if (a == b || b==c || a==c) {
+                } else if (a == b || b == c || a == c) {
                     System.out.println("Triangle: isosceles");
                     break;
                 } else {
                     System.out.println("Triangle: scalene");
                     break;
                 }
-            }else{
+            } else {
                 System.out.println("Values are not correct!");
                 break;
             }
 
-        }while(a>0 && b>0 && c>0);
+        } while (a > 0 && b > 0 && c > 0);
+    }
+}*/
+        Scanner input = new Scanner(System.in);
+
+        double a, b, c;
+        do {
+            a = input.nextDouble();
+            b = input.nextDouble();
+            c = input.nextDouble();
+
+            boolean correct = a > 0 && b > 0 && c > 0 && a + b > c && a + c > b && b + c > a;
+            if (correct) {
+                double s = (a + b + c) / 2;
+                double area = Math.sqrt(s * (s - a) * (s - b) * (s - c));
+                System.out.println("Area: " + area);
+
+                if (a == b && b == c) {
+                    System.out.println("Triangle: equilateral");
+                } else if (a == b || a == c || b == c) {
+                    System.out.println("Triangle: isosceles");
+                } else {
+                    System.out.println("Triangle: scalene");
+                }
 
 
+            } else {
+                System.out.println("Values are not correct!");
+            }
+        } while (a > 0 && b > 0 && c > 0);
     }
 }
+
+
+
+
+
